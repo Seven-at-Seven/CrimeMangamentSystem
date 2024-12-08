313331dd9a522b5f95524f0d9750f7829a853213
Omar Ehab <amooraomar2005@gmail.com>
2024-12-08 17:51:49 +0200

    version-1 of policeOfficer class

--- src/main/java/com/sevenatseven/mainentities/PoliceOfficer.java
+++ src/main/java/com/sevenatseven/mainentities/PoliceOfficer.java
@@ -1,5 +1,33 @@
 package com.sevenatseven.mainentities;
 
-public class PoliceOfficer {
+public class PoliceOfficer extends Person {
+    private String phoneNumber;
+    private int salary;
+    private String rank;
+    private String BadgeNumber;
+
+    PoliceOfficer (String firstName, String lastName,String nationalId, String phoneNumber, int salary, String rank,String BadgeNumber) {
+        super(firstName, lastName, nationalId);
+        this.salary = salary;
+        this.phoneNumber = phoneNumber;
+    this.BadgeNumber = BadgeNumber;
+    }
+
+    public void setSalary (int salary,boolean isAdmin) {
+       if (isAdmin)this.salary = salary;
+    }
+    public int getSalary () {
+        return salary;
+    }
+
+
+
+    public void setRank (String rank,boolean isAdmin) {
+        if (isAdmin)this.rank = rank;
+    }
+    public String getRank () {
+        return rank;
+    }
+
 
 }

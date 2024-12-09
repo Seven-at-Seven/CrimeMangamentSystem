fbc80b56216474da04228a51e2457a1e78202dfc
Omar M. Hasan <hilariousbananas@proton.me>
2024-12-09 16:39:25 +0200

    merged

--- src/main/java/com/sevenatseven/mainentities/Person.java
+++ src/main/java/com/sevenatseven/mainentities/Person.java
@@ -1,29 +1,48 @@
+package com.sevenatseven.mainEntities;
 
-package com.sevenatseven.mainentities;
+abstract public class Person { // will use the setters for updating
+    private String firstName;
+    private String lastName;
+    private String id;
+    private String national_id;
 
-import org.w3c.dom.ls.LSOutput;
-
-abstract public class Person {
-private String firstName;
-private String lastName;
-private String nationalId;
-
-   public Person (String firstName,String lastName, String nationalId) {
-   this.firstName = firstName;
-   this.lastName = lastName;
-   this.nationalId = nationalId;
-   }
-
-    String getFirstName () {
-       return firstName;
+    public Person(String firstName, String lastName, String id, String national_id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.national_id = national_id;
     }
 
-    String getLastName () {
-       return lastName;
+    public String get_name() {
+        return firstName + " " + lastName;
     }
 
-    String getNationalId () {
-   return nationalId;
-   }
+    public String get_id() {
+        return id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationalId() {
+        return national_id;
+    }
+
+    public void setNationalId(String national_id) {
+        this.national_id = national_id;
+    }
 }
+

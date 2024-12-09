ed08e14d20ec4eab12de8a9ad9dfee1294a46011
Omar Ehab <amooraomar2005@gmail.com>
2024-12-09 16:54:54 +0200

    added email and password to policeofficer

--- src/main/java/com/sevenatseven/mainentities/PoliceOfficer.java
+++ src/main/java/com/sevenatseven/mainentities/PoliceOfficer.java
@@ -2,15 +2,19 @@ package com.sevenatseven.mainentities;
 
 public class PoliceOfficer extends Person {
     private String phoneNumber;
+    private String email;
+    private String password;
     private int salary;
     private String rank;
     private Department department;
 
-    PoliceOfficer (String firstName, String lastName,String id,String nationalId,  int salary,String phoneNumber, String rank) {
-        super(firstName, lastName,id, nationalId);
+    PoliceOfficer (String firstName, String lastName, String id, String nationalId, int salary, String phoneNumber, String rank, String email, String password) {
+        super(firstName, lastName, id, nationalId);
         this.salary = salary;
         this.phoneNumber = phoneNumber;
         this.rank = rank;
+        this.email = email;
+        this.password = password;
 
     }
 
